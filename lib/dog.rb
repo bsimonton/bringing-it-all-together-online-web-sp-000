d57fa77b10e56ba20a5f662368f55fe2d8b296c9
Brian Simonton <bsimonton78@gmail.com>
2019-04-09 23:48:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,10 @@
 class Dog 
-  
+ attr_accessor :id, :name, :breed
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end 
   
   
   
